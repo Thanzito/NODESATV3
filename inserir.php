6bf5faee7a9e5f0bee1bb693c56c538f5abbbcfd
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-06-17 12:13:17 -0300

    Add files via upload

---
+++ inserir.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+// Verificar se os dados foram enviados via POST
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Conectar com o banco
+    $conn = new mysqli ("localhost","root","","panificadora");
+
+    if($conn->connect_error) {
+        die(json_encode(["erro" => "Erro ao conectar"]));
+    }
+
+    //Receber e limpar os dados
+    $nome = $conn -> real_escape_string($_POST['nome']);
+    $matricula = $conn -> real_escape_string($_POST['nome']);
+    $funcao = $conn -> real_escape_string($_POST['nome']);
+
+    //Inserir no banco
+    $sql = "INSERT INTO usuarios (ID, nome, matricula, funcao) VALUES('$nome', '$matricula', '$funcao')";
+    if($conn -> query($sql)){
+        echo json_encode(["sucesso" =>true,"mensagem" =>"Usuário inserido com sucesso!"]);
+    } else {
+        echo json_encode(["erro" => "Erro ao inserir usuario."]);
+    }
+    $conn->close();
+} else {
+    echo json_encode(["erro" => "Requisição inválida."]);
+}
